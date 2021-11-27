dbcdcf35773b633afd7808531f802c46b8081c7d
Oisín_windows <byrne.ois@gmail.com>
2021-11-27 12:51:23 +0000

    score page styling

--- src/components/Scoring.js
+++ src/components/Scoring.js
@@ -1,20 +1,20 @@
 const Scoring = () => {
     return ( 
         
-        <div className="h-full flex flex-wrap justify-evenly pt-10 bg-leinster-blue">
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Tryscorer <span  className="text-leinster-gold">+3</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Last Tryscorer <span className="text-leinster-gold">+4</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Kick Success <span  className="text-leinster-gold">+1</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Kick Failure <span  className="text-leinster-gold">-2</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Drop Goal <span  className="text-leinster-gold">+2</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Failed Drop Goal<span  className="text-leinster-gold">-2</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Best Assist <span  className="text-leinster-gold">+4</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Shit the bed <span  className="text-leinster-gold">-1</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Yellow Card <span  className="text-leinster-gold">-3</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Red Card <span  className="text-leinster-gold">-4</span></p>
-            <p className="w-full text-white font-jersey text-xl uppercase">Offical Man Of the Match <span  className="text-leinster-gold">+1</span></p>
-            <p className="w-full text-white font-jersey text-xl uppercase">Nickelodeon Teen Choice Award <span  className="text-leinster-gold">+1</span></p>
-            <p className="w-1/2 text-white font-jersey text-xl uppercase">Blue Magic <span  className="text-leinster-gold">+4</span></p>
+        <div className="h-full flex flex-wrap justify-evenly pt-10 px-4 bg-leinster-blue">
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Tryscorer <span  className="text-leinster-gold">+3</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Last Tryscorer <span className="text-leinster-gold">+4</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Kick Success <span  className="text-leinster-gold">+1</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Kick Failure <span  className="text-leinster-gold">-2</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Drop Goal <span  className="text-leinster-gold">+2</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Failed Drop Goal<span  className="text-leinster-gold">-2</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Best Assist <span  className="text-leinster-gold">+4</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Shit the bed <span  className="text-leinster-gold">-1</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Yellow Card <span  className="text-leinster-gold">-3</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Red Card <span  className="text-leinster-gold">-4</span></p>
+            <p className="w-full text-white font-jersey text-2xl uppercase">Offical Man Of the Match <span  className="text-leinster-gold">+1</span></p>
+            <p className="w-full text-white font-jersey text-2xl uppercase">Nickelodeon Teen Choice Award <span  className="text-leinster-gold">+1</span></p>
+            <p className="w-1/2 text-white font-jersey text-2xl uppercase">Blue Magic <span  className="text-leinster-gold">+4</span></p>
         </div>
 
      );
